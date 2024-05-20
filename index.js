40402acb0093e5e282bdc4c8d3f1b5b3dd3ceed7
yachika singh <152047557+yachikasingh@users.noreply.github.com>
2024-05-20 09:48:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// console.log("now i'am linked");
+
+
+
+let outputScreen = document.getElementById("output-screen");
+
+function display(num){
+    outputScreen.value += num;
+}
+
+
+function calculate(){
+    try{
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch(err)
+    {
+        alert("Invalid")
+    }
+}
+
+
+function Clear(){
+    outputScreen.value ="";
+}
+
+function del(){
+    outputScreen.value = outputScreen.value.slice(0,-1);
+}
